cfd1eb6857d562cb7c26dd5ac907866ea4f35e6d
Stas Petrov <petrovstas@mail.ru>
2023-10-08 12:22:21 +0500

    Added setting for Pane

--- src/main/java/grapher/interactor/services/draw/PaneJavaFX.java
+++ src/main/java/grapher/interactor/services/draw/PaneJavaFX.java
@@ -6,6 +6,11 @@ import javafx.scene.shape.Shape;
 
 public class PaneJavaFX extends Pane implements ICanvas {
 
+    public PaneJavaFX() {
+        setMaxSize(1000, 700);
+        setStyle("-fx-border-width: 5; -fx-border-color: gray;");
+    }
+
     @Override
     public void add(Shape shape) {
         this.getChildren().add(shape);
